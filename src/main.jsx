cadbe60a61ecb8c254c78a46e32c58e2cb977cc4
ungerjulia <42190599+ungerjulia@users.noreply.github.com>
2026-04-01 16:47:28 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MediaEditor from './MediaEditor.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MediaEditor />
+  </React.StrictMode>
+)
