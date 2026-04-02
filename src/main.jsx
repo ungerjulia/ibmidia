c807ed7eee626cdd1b3a22107b72c8d35eb8dc56
ungerjulia <42190599+ungerjulia@users.noreply.github.com>
2026-04-02 08:34:30 -0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MediaEditor from './MediaEditor.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MediaEditor />
-  </React.StrictMode>
-)
